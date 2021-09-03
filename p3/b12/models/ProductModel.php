1e0024493cac88a5e09e37e465f1500750e29278
chungnn <chungnguyen2010@gmail.com>
2021-09-03 10:09:38 +0700

    add p3 b12

---
+++ p3/b12/models/ProductModel.php
@@ -0,0 +1,24 @@
+<?php 
+require_once APP_DIR . DS . "libs" . DS . "database.php";
+
+class ProductModel {
+
+    public function getProducts() {
+        $sql = 'SELECT * FROM products ORDER BY id DESC';
+        $result = Database::getConnection()->query($sql, PDO::FETCH_ASSOC);
+        $return = [];
+        foreach ( $result as $row) {
+            $return[] = $row;
+        }
+        return $return;
+    }
+
+    public function getProduct($id) {
+        $sql = "SELECT id, price FROM products WHERE id = :id";
+        $stmt = Database::getConnection()->prepare($sql);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        return $stmt->fetchObject();
+    }
+
+}
